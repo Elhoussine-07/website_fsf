c9180f0e439c19512851553315d335557d887f3d
Elhoussine-07 <ellahoussine1@gmail.com>
2025-02-04 14:09:32 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/website_fsf/', 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
